a436898c067d3f5184d37b7039990717c1a011ce
hardik23682 <hardik.rkcet@gmail.com>
2022-04-28 11:47:36 +0530

    Initial Commit

---
+++ AutoOTP/AutoOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTPApp.swift
+//  AutoOTP
+//
+//  Created by Chirag on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
